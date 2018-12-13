c7ebf2d709f1ea1a1da0e09cccd199c09474cc5c
Mohammad Salehi <msalehy77@gmail.com>
2018-12-13 14:48:31 +0330

    missing Place model

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+const Place = require("./../database/models/place");
+
 router.get("/", function(req, res, next) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
